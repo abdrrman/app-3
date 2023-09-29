f98b44fb1b4aa649ca35506430462c7767dc84c5
abdrrman <ubuntu@ip-172-31-42-248.eu-central-1.compute.internal>
2023-09-29 12:19:56 +0000

    req

--- 39.py
+++ 39.py
@@ -1,3 +1,81 @@
+# All library imports
 
+import os
+import shutil
 import streamlit as st
-st.title('app-3-39')
+from langchain import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (ChatPromptTemplate,
+                                    HumanMessagePromptTemplate,
+                                    SystemMessagePromptTemplate)
+from langchain.document_loaders import *
+from langchain.chains.summarize import load_summarize_chain
+import tempfile
+from langchain.docstore.document import Document
+import time
+from langchain.memory import ConversationBufferMemory
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+import sqlite3
+import stripe
+
+# Get openai_api_key
+openai_api_key = st.sidebar.text_input(
+    "OpenAI API Key",
+    placeholder="sk-...",
+    value=os.getenv("OPENAI_API_KEY", ""),
+    type="password",
+)
+
+### Copy and paste all the functions as is
+
+def create_svelte_app():
+    # Your code here
+    
+def create_sqlite_database():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('user_logins.db')
+    
+    # Create a cursor object to execute SQL queries
+    cursor = conn.cursor()
+    
+    # Create a table to store user logins
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS user_logins (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+def create_stripe_integration():
+    # Set your Stripe API key
+    stripe.api_key = 'your_stripe_api_key'
+    
+    # Create a Stripe integration
+    stripe_integration = stripe.Integration.create(
+        name='My Stripe Integration',
+        type='payment',
+        enabled=True
+    )
+    
+    return stripe_integration
+
+### Create a form
+
+with st.form(key='story_game'):
+    # Under the form, take all the user inputs
+    create_svelte_app()
+    create_sqlite_database()
+    create_stripe_integration()
+    submit_button = st.form_submit_button(label='Submit Story')
+    # If form is submitted by st.form_submit_button run the logic
+    if submit_button:
+        #Under the st.form_submit_button, show the results.
+        st.title('waveboundsounds')
+        sqlite_database = 'user_logins.db'
+        stripe_integration = create_stripe_integration()
