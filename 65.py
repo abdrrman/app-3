ab1a1880a0092bf7d95242138ec796acad34de30
abdrrman <ubuntu@ip-172-31-42-248.eu-central-1.compute.internal>
2023-10-06 12:25:51 +0000

    req

--- 65.py
+++ 65.py
@@ -1,3 +1,64 @@
+# All library imports
 
+import os
+import shutil
 import streamlit as st
-st.title('app-3-65')
+from langchain import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import (ChatPromptTemplate,
+                                    HumanMessagePromptTemplate,
+                                    SystemMessagePromptTemplate)
+from langchain.document_loaders import *
+from langchain.chains.summarize import load_summarize_chain
+import tempfile
+from langchain.docstore.document import Document
+import time
+from langchain.memory import ConversationBufferMemory
+from langchain.chains.question_answering import load_qa_chain
+
+# Get openai_api_key
+openai_api_key = st.sidebar.text_input(
+    "OpenAI API Key",
+    placeholder="sk-...",
+    value=os.getenv("OPENAI_API_KEY", ""),
+    type="password",
+)
+
+### Copy and paste all the functions as is
+
+def promptWordGenerator(text):
+    chat = ChatOpenAI(
+        model="gpt-3.5-turbo-16k",
+        openai_api_key=openai_api_key,
+        temperature=0.7
+    )
+    system_template = """You are an AI language model trained to generate prompt words based on the given input text."""
+    system_message_prompt = SystemMessagePromptTemplate.from_template(system_template)
+    human_template = """Please generate prompt words based on the following text: '{text}'."""
+    human_message_prompt = HumanMessagePromptTemplate.from_template(human_template)
+    chat_prompt = ChatPromptTemplate.from_messages(
+        [system_message_prompt, human_message_prompt]
+    )
+
+    chain = LLMChain(llm=chat, prompt=chat_prompt)
+    result = chain.run(text=text)
+    return result # returns string   
+
+### Create a form
+
+with st.form(key='prompt_word_generator'):
+    # Under the form, take all the user inputs
+    text = st.text_input("Enter input text")
+    submit_button = st.form_submit_button(label='Generate Prompt Words')
+    # If form is submitted by st.form_submit_button run the logic
+    if submit_button:
+        if not openai_api_key.startswith('sk-'):
+            st.warning('Please enter your OpenAI API key!', icon='⚠')
+            prompt = ""
+        elif text:
+            prompt = promptWordGenerator(text)
+        else:
+            prompt = ""
+        #Under the st.form_submit_button, show the results.
+        if prompt:
+            st.info(prompt)
